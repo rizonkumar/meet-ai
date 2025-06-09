dbac50952c4d0d884d7c953fbd4fa8d501b5e99a
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-09 07:17:31 +0530

    Feat: Implemented Better Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,60 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+
 export default function Home() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  const { data: session } = authClient.useSession();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Error");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Welcome {session.user?.name}</p>
+      </div>
+    );
+  }
+  return (
+    <div className="p-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Sign In</Button>
+    </div>
+  );
 }
