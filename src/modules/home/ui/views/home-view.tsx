9d66bd9677d6c80ee8646237ccd7e97fc510533a
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-20 23:15:59 +0530

    Enhance sign-out functionality in HomeView to include redirection to sign-in page upon successful logout

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -2,14 +2,27 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export const HomeView = () => {
   const { data: session } = authClient.useSession();
-
+  const router = useRouter();
   return (
     <div className="flex flex-col p-4 gap-y-4">
       <p>Logged in as {session?.user?.name}</p>
-      <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      <Button
+        onClick={() =>
+          authClient.signOut({
+            fetchOptions: {
+              onSuccess: () => {
+                router.push("/sign-in");
+              },
+            },
+          })
+        }
+      >
+        Sign Out
+      </Button>
     </div>
   );
 };
