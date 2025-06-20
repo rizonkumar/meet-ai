943a8a149522271526147a11c1862c6717d37665
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-20 16:09:02 +0530

    Refactor: Update authentication pages to use async session checks and redirect logic, and create HomeView component for logged-in users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,18 @@
-"use client";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { useState } from "react";
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-export default function Home() {
-  const { data: session } = authClient.useSession();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-
-  const onSubmit = () => {
-    authClient.signUp.email(
-      {
-        email,
-        name,
-        password,
-      },
-      {
-        onError: () => {
-          window.alert("Error");
-        },
-        onSuccess: () => {
-          window.alert("Success");
-        },
-      }
-    );
-  };
-
-  if (session) {
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <p>Welcome {session.user?.name}</p>
-      </div>
-    );
+  if (!session) {
+    redirect("/sign-in");
   }
-  return (
-    <div className="p-4">
-      <Input
-        placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onSubmit}>Sign In</Button>
-    </div>
-  );
-}
+
+  return <HomeView />;
+};
+
+export default Page;
